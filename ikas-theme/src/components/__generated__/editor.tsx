acf79656c8e90e775fe7c5e3cef17efa2c1ee8ec
Said Furkan <dizefurkan@gmail.com>
2022-08-03 15:59:21 +0300

    [DELETE] delete start component

--- ikas-theme/src/components/__generated__/editor.tsx
+++ ikas-theme/src/components/__generated__/editor.tsx
@@ -2,14 +2,13 @@ import dynamic from "next/dynamic";
 import { IkasEditorComponentLoader } from "@ikas/storefront";
 
 
-const Component0 = dynamic(() => import("../start"), { loading: () => <IkasEditorComponentLoader /> });
-const Component1 = dynamic(() => import("../hero-banner"), { loading: () => <IkasEditorComponentLoader /> });
-const Component2 = dynamic(() => import("../header"), { loading: () => <IkasEditorComponentLoader /> });
-const Component3 = dynamic(() => import("../footer"), { loading: () => <IkasEditorComponentLoader /> });
+const Component0 = dynamic(() => import("../hero-banner"), { loading: () => <IkasEditorComponentLoader /> });
+const Component1 = dynamic(() => import("../header"), { loading: () => <IkasEditorComponentLoader /> });
+const Component2 = dynamic(() => import("../footer"), { loading: () => <IkasEditorComponentLoader /> });
 
 
 const Components = {
-  "28626d46-97f8-45e7-9c25-dcb16d618676": Component0,"8b5d6278-a490-4f0a-8308-00bf888b79b0": Component1,"ae2074dc-577e-40a1-a9aa-6104aaacdb40": Component2,"a1ea193f-50da-48a3-baa7-5fa8b9ec5b41": Component3
+  "8b5d6278-a490-4f0a-8308-00bf888b79b0": Component0,"ae2074dc-577e-40a1-a9aa-6104aaacdb40": Component1,"a1ea193f-50da-48a3-baa7-5fa8b9ec5b41": Component2
 };
 
 export default Components;
