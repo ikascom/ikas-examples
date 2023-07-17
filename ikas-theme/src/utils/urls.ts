d74c6240b2bf0d61dbafc1a7697b861525f118b4
celebitolga <tolgacelebi78@gmail.com>
2023-07-17 17:33:13 +0300

    [ADD] Urls to utils

---
+++ ikas-theme/src/utils/urls.ts
@@ -0,0 +1,15 @@
+const urls = {
+  base: "/",
+  cart: "/cart",
+  search: "/search",
+  blog: "/blog",
+  account: "/account",
+  accountFavorite: "/account/favorite-products",
+  accountAddresses: "/account/addresses",
+  accountOrders: "/account/orders",
+  login: "/account/login",
+  register: "/account/register",
+  forgotPassword: "/account/forgot-password",
+};
+
+export default urls;
